7172371e07bcaada88e849731bec3e016f7ee186
Scovia Achan <achanscovia@gmail.com>
2020-09-18 22:13:26 +0300

    Styling page

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+((scovia) => {
+    "use strict";
+    function O(element){
+        return scovia.getElementById(element)
+    }
+
+    function S(element){
+        return O(element).style
+    }
+
+    function C(element){
+        return scovia.getElementsByClassName(element)
+    }
+
+    S("scovia").color = '#F00'
+})(document);
