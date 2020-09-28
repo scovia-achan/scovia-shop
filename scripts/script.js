e98020308ac6b06b9bd92f1cc0a64036ba11608e
Scovia Achan <achanscovia@gmail.com>
2020-09-28 22:57:44 +0300

    Changed footer color removed Contacts heading

--- scripts/script.js
+++ scripts/script.js
@@ -15,10 +15,3 @@
     // S("scovia").color = '#F00'
 })(document);
 
-// function myMap(){
-//     var mapProp={
-//         center:new google.maps.LatLong(51.508742,-0.120850),
-//         zoom:5
-//     }
-//     var map=new google.maps.Map(document.getElementsByClassName("google-map"), mapProp)
-// }
